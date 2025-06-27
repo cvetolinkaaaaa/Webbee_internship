d22474142cb4ffec7a8428d50034115ff5fc2c95
cvetolinkaaaaa <c.evseeva05@mail.ru>
2025-06-27 23:34:16 +0300

    docs: add javadoc for proxy classes

--- java-patterns/src/main/java/org/example/proxy/ProxyService.java
+++ java-patterns/src/main/java/org/example/proxy/ProxyService.java
@@ -3,11 +3,23 @@ package org.example.proxy;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Класс ProxyService реализует паттерн Proxy.
+ */
+
 public class ProxyService implements Service {
 
     private static final Logger LOGGER = LogManager.getLogger(ProxyService.class);
+
+    /**
+     * Ссылка на реальную реализацию сервиса.
+     */
     private ServiceRealisation serviceRealisation;
 
+    /**
+     * При первичном вызове метода инициализируется сервис,
+     * при последующих вызовах метода новый объект не будет инициализироваться.
+     */
     @Override
     public void sendMessage() {
         LOGGER.info("Проверка через прокси");
--- java-patterns/src/main/java/org/example/proxy/ServiceRealisation.java
+++ java-patterns/src/main/java/org/example/proxy/ServiceRealisation.java
@@ -3,10 +3,18 @@ package org.example.proxy;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Класс, реализующий метод сервиса.
+ * @author Evseeva Tsvetolina
+ */
+
 public class ServiceRealisation implements Service {
 
     private static final Logger LOGGER = LogManager.getLogger(ServiceRealisation.class);
 
+    /**
+     * Реализаия отправления сообщения.
+     */
     @Override
     public void sendMessage() {
         LOGGER.info("Отправка сообщения");
