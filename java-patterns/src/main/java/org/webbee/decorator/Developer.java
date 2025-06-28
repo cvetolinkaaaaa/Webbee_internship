9f4dbe0a842ea67a50b0b9027634cc5659f3185e
cvetolinkaaaaa <c.evseeva05@mail.ru>
2025-06-28 21:56:18 +0300

    Добавлена реализация паттерна Декоратор
    * add pattern decorator
    
    * add feature/decorator
    
    * add javadoc
    
    * delete main
    
    * chore: update project structure
    
    * docs: change readme

---
+++ java-patterns/src/main/java/org/webbee/decorator/Developer.java
@@ -0,0 +1,15 @@
+package org.webbee.decorator;
+
+/**
+ * Интерфейс Developer представляющий разработчика, который может выполнить задачи по написанию кода.
+ * Является реализацией паттерна Decorator.
+ * @author Evseeva Tsvetolina
+ */
+public interface Developer {
+
+    /**
+     * Выполняет задачу по написанию кода.
+     */
+    String develop();
+
+}
