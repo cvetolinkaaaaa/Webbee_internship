eb4c1a5d07dfd59fd1d1e6008555730fc2a571fc
cvetolinkaaaaa <c.evseeva05@mail.ru>
2025-06-28 22:02:32 +0300

    feat: add pattern adapter
    * add pattern decorator
    
    * add feature/decorator
    
    * add javadoc
    
    * add feature/adapter
    
    * add javadoc
    
    * chore: update project structure
    
    * chore: update project structure
    
    * docs: change readme

---
+++ java-patterns/src/main/java/org/webbee/adapter/USB.java
@@ -0,0 +1,16 @@
+package org.webbee.adapter;
+
+/**
+ * Интерфейс, представляющий USB-устройство.
+ * Определяет метод подключения устройства через USB-кабель.
+ * @author Evseeva Tsvetolinа
+ */
+
+public interface USB {
+
+    /**
+     * Подключает устройство через USB-кабель.
+     */
+    void connectViaUsb();
+
+}
